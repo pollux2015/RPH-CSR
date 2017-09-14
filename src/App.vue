26756dd0a9e86776b1f98f3ddce526ce2d04edad
pollux <pollux@polluxMac.local>
2017-09-14 11:44:30 +0800

    提价line

--- src/App.vue
+++ src/App.vue
@@ -60,4 +60,39 @@ body {
   overflow-x: hidden;
 }
 
+.line-before,
+.line-after{
+  position: relative;
+}
+.line-before:before,
+.line-after:after{
+    content: " ";
+    position: absolute;
+    left: 0;
+    right: 0;
+    height: 1px;
+    color: #C0BFC4;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+    -webkit-transform: scaleY(0.5);
+    transform: scaleY(0.5);
+}
+
+.line-before:before,
+.line-after:after{
+  border-top: 1px solid #C0BFC4;
+}
+
+.line-before:before{
+    top: 0;
+}
+
+.line-after:after{
+    bottom: 0;
+}
+.line-before-dot:before,
+.line-after-dot:after{
+  border-top-style: dashed;
+}
+
 </style>
